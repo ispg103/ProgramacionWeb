2aa01ea62c476d31178570487c08d69d685187b1
Your Name <you@example.com>
2024-02-01 19:03:36 -0500

    starting react

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export function Body(){
+    return(
+        <main>
+            <h2>This is body</h2>
+        </main>
+    )
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export function Header(){
+    return(
+        <header>
+            <h1>Holaaaa</h1>
+        </header>
+    )
+}
