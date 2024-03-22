f517bc5b4362fd100aa726f9a67c2f3c23e48939
LauM727 <LauraMarcelaM.mp26@gmail.com>
2024-03-22 18:56:18 -0500

    Remove <React.StrictMode> of Main

--- todoapp/src/main.jsx
+++ todoapp/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { TasksProvider } from './context/TaskContextProvider.jsx';
@@ -6,9 +5,9 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-  <React.StrictMode>
+
     <TasksProvider>
       <App />
     </TasksProvider>
-  </React.StrictMode>,
+
 );
